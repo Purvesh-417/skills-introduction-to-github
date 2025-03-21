2941c649c57086d8dd7879ee536a7188fd7fc8eb
purvesh-417 <ppurvesh50@gmail.com>
2025-03-21 21:46:26 +0530

    day1py

---
+++ bandname.py
@@ -0,0 +1,4 @@
+print("welcome to the band generator")
+city=input("whats the name of the city you grew up in?\n")
+pet=input("what is the name of your pet\n")
+print("your band name could be"+" "+city+pet)
